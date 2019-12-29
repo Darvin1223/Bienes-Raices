8363252523e163a9b3e5f79da6bc43aa43aadeb9
Darvin <Darvin.rod10@gmail.com>
2019-12-29 19:42:42 -0400

    Changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,39 @@
 (function() {
-    "Use strict";
-    document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener("DOMContentLoaded", function() {
+
+        /** Contactanos **/
+        // let opciones = document.getElementsByName('Eleccion'),
+        //     Alquiler = document.getElementById('Alquiler'),
+        //     Compra_venta = document.getElementById('Comven'),
+        //     fieldset = document.getElementsByTagName('fieldset');
+
+
+        // function Opciones() {
+
+        //     console.log(opciones);
+        //     for (let i = 0; i < opciones.length; i++) {
+        //         if (opciones[i].checked) {
+        //             console.log(opciones[i].value);
+        //         }
+        //     }
+
+        // };
+        // document.addEventListener("click", function(Opciones) {
+        //     if (Opciones == Compra_venta) {
+        //         fieldset.ClassList.remove("hiden");
+        //     }
+
+        // })
+
+        $("#my-form").submit(function(e) {
+            e.preventDefault();
+
+            var $form = $(this);
+            $.post($form.attr("action"), $form.serialize()).then(function() {
+                alert("Thank you!");
+            });
+        });
+
 
     });
 
